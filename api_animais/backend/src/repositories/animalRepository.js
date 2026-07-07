584841127b4fce7548676dedc8014de5904f018b
mariangel <hernandezmariangel006@gmail.com>
2026-07-07 17:15:17 -0300

    atualizar

--- api_animais/backend/src/repositories/animalRepository.js
+++ api_animais/backend/src/repositories/animalRepository.js
@@ -18,7 +18,7 @@ export const animalRepository = {
     },
 
     async update(id, animal) {
-          const sql = 'UPDATE (nome, especie, idade, status_saude) VALUES($1, $2, $3, $4) RETURNING *;'; 
+          const sql = 'UPDATE animal SET nome = $1, especie = $2, idade = $3, status_saude = $4) RETURNING *;'; 
           const res = await query(sql, [nome, especie, idade, status_saude]);
           return res.rows[0];
     }
